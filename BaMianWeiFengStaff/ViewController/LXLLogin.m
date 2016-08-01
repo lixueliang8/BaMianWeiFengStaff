bfb163f9be17fcd6c6cbc76352c4836430cc6e92
lixueliang8 <lixueliang@lixueliangdeMac-mini.local>
2016-08-01 16:09:30 +0800

    diyici
    1

---
+++ BaMianWeiFengStaff/ViewController/LXLLogin.m
@@ -0,0 +1,16 @@
+//
+//  LXLLogin.m
+//  BaMianWeiFengStaff
+//
+//  Created by 李学良 on 16/7/30.
+//  Copyright © 2016年 LiXueLiang. All rights reserved.
+//
+
+#import "LXLLogin.h"
+
+@implementation LXLLogin
+- (void)viewDidLoad
+{
+    self.view.backgroundColor = [UIColor redColor];
+}
+@end
